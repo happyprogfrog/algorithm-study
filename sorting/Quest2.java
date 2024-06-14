db588da2bc690efd7d8dcf7cab081f9dc9cc0c9a
happyprogfrog <happyprogfrog@gmail.com>
2024-06-14 22:18:37 +0900

    style: { 위치 변경

--- sorting/Quest2.java
+++ sorting/Quest2.java
@@ -26,8 +26,7 @@ public class Quest2 {
         boolean is_swap = false;
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j + 1] < arr[j])
-                {
+                if (arr[j + 1] < arr[j]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
